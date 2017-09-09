0b9993695afe9d79d1070585d8605bd89a00785d
LiuQingying <648501626@qq.com>
2017-09-09 16:51:01 +0800

    提交控制器

---
+++ JMWebViewController/JMWebViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JMWebViewController
+//
+//  Created by LiuQingying on 2017/9/9.
+//  Copyright © 2017年 LiuQingying. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
